944280eb7d61cec9195fbf746bd6887e671d4cee
arismoraitis <aris.moraiths@gmail.com>
2025-11-09 15:20:04 +0200

    feat: learning group related state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
+// import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+// import Counter from "./components/Counter.tsx";
+// import NameChanger from "./components/NameChanger.tsx";
+// import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
+import CounterAdvanced from "./components/CounterAdvanced.tsx";
 import Layout from "./components/Layout.tsx";
-import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
-import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
-import Counter from "./components/Counter.tsx";
 
 function App() {
 
@@ -15,18 +18,24 @@ function App() {
 
         <Layout>
 
-<Counter/>
-           <ClassComponentWithState/>
-            <FunctionalComponentWithState/>
-            <FunctionalComponent/>
-        <ClassComponent/>
-        <FunctionalComponent/>
-        <ArrowFunctionalComponent/>
-        <ArrowFunctionalComponentWithProps title="Is a Arrow Component with Props!"/>
-        <ArrowFunctionalComponentWithPropsType
-            title="Is a Arrow Component with Props!"
-            description="This is a description."
-        />
+
+<CounterAdvanced/>
+{/*<CounterWithMoreStates/>*/}
+
+{/*            <NameChanger/>*/}
+
+{/*<Counter/>*/}
+{/*           <ClassComponentWithState/>*/}
+{/*            <FunctionalComponentWithState/>*/}
+{/*            <FunctionalComponent/>*/}
+{/*        <ClassComponent/>*/}
+{/*        <FunctionalComponent/>*/}
+{/*        <ArrowFunctionalComponent/>*/}
+{/*        <ArrowFunctionalComponentWithProps title="Is a Arrow Component with Props!"/>*/}
+{/*        <ArrowFunctionalComponentWithPropsType*/}
+{/*            title="Is a Arrow Component with Props!"*/}
+{/*            description="This is a description."*/}
+{/*        />*/}
         </Layout>
     </>
   )
